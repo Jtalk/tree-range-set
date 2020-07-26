a0ecd260adeb67121a47fed176e54be5b6967a4b
Roman Nazarenko <me@jtalk.me>
2020-07-26 16:34:35 +0100

    Exclude spec files from prod builds

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ["src"],
-  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  testMatch: ["**/?(*.)+(spec).+(ts|tsx|js)"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
