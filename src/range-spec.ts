dde0c37cbd4a39b6f5363145bcfbbcb868d86ebf
Roman Nazarenko <me@jtalk.me>
2020-08-03 22:01:25 +0100

    Derive RangeSpec comparison from a comparator

--- src/range-spec.ts
+++ src/range-spec.ts
@@ -11,6 +11,8 @@ export interface RangeSpec<T> {
   isLessThan(value: T, than: T): boolean;
   isGreaterOrEqualTo(value: T, thanTo: T): boolean;
   isLessOrEqualTo(value: T, thanTo: T): boolean;
+
+  comparator: (a: T, b: T) => number;
 }
 
 export abstract class AbstractRangeSpec<T> implements RangeSpec<T> {
@@ -18,20 +20,24 @@ export abstract class AbstractRangeSpec<T> implements RangeSpec<T> {
   abstract plus(a: T, b: T): T;
   abstract minus(a: T, b: T): T;
 
-  abstract isEqual(a: T, b: T): boolean;
+  abstract get comparator(): (a: T, b: T) => number;
+
   abstract isInfinity(value: T): boolean;
-  abstract isGreaterThan(value: T, than: T): boolean;
 
+  isEqual(a: T, b: T): boolean {
+    return this.comparator(a, b) === 0;
+  }
+  isGreaterThan(value: T, than: T): boolean {
+    return this.comparator(value, than) > 0;
+  }
   isGreaterOrEqualTo(value: T, thanTo: T): boolean {
-    return this.isEqual(value, thanTo) || this.isGreaterThan(value, thanTo);
+    return this.comparator(value, thanTo) >= 0;
   }
-
   isLessOrEqualTo(value: T, thanTo: T): boolean {
-    return this.isEqual(value, thanTo) || this.isLessThan(value, thanTo);
+    return this.comparator(value, thanTo) <= 0;
   }
-
   isLessThan(value: T, than: T): boolean {
-    return this.isGreaterThan(than, value);
+    return this.comparator(value, than) < 0;
   }
 
   max(a: T, b: T): T {
@@ -53,13 +59,13 @@ export class NumberSpec extends AbstractRangeSpec<number> {
   minus(a: number, b: number): number {
     return a - b;
   }
-  isEqual(a: number, b: number): boolean {
-    return a === b;
+  get comparator(): (a: number, b: number) => number {
+    return (a, b) => {
+      if (a === b) return 0; // Infinity case should return 0 here
+      return a - b;
+    };
   }
   isInfinity(value: number): boolean {
     return +value === Infinity || -value === Infinity;
-  }
-  isGreaterThan(value: number, than: number): boolean {
-    return value > than;
   }
 }
