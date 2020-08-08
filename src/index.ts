9861af80a81dea8ba7b8c41cf5df200b72bd0904
Roman Nazarenko <me@jtalk.me>
2020-08-08 15:47:04 +0100

    Module exports setup

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { RangeSet } from "./range-set";
+export { RangeSetTree, TreeIterator, Comparator } from "./range-set-tree";
+export { Range } from "./range";
+export { RangeSpec, AbstractRangeSpec, NumberSpec } from "./range-spec.js";
